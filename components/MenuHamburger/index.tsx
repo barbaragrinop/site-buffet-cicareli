0e61b06ccbac8b98078580a5e79e2cef20e73161
Barbara Hellen <barbara.pereira@modalgr.com.br>
2023-01-02 11:36:10 -0300

    test: blur on safari

--- components/MenuHamburger/index.tsx
+++ components/MenuHamburger/index.tsx
@@ -1,26 +1,51 @@
-import { Menu } from "@headlessui/react";
 import classNames from "classnames";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import React, { useState } from "react";
-import MenuComBarras from "../../public/icons/menu-hamburger";
+import { ItemsMenu } from "../../utils/items-menu";
 import styles from "./MenuHamburger.module.scss";
 
 export default function MenuHamburger() {
+  const isActive = document.querySelectorAll(".active");
+  console.log("isActive", isActive);
+
   const [isShow, setIsShow] = useState<boolean>(false);
+  const { pathname } = useRouter();
 
   return (
-    <div
-      className={`
+    <>
+      <div
+        className={`
         ${
           isShow
             ? classNames(styles.hamburger, styles.active)
             : styles.hamburger
         } `}
-      // className={styles.hamburger}
-      onClick={() => setIsShow(!isShow)}
-    >
-      <span className={classNames(styles.line, styles.line1)}></span>
-      <span className={classNames(styles.line, styles.line2)}></span>
-      <span className={classNames(styles.line, styles.line3)}></span>
-    </div>
+        // className={styles.hamburger}
+        onClick={() => setIsShow(!isShow)}
+      >
+        <span className={classNames(styles.line, styles.line1)}></span>
+        <span className={classNames(styles.line, styles.line2)}></span>
+        <span className={classNames(styles.line, styles.line3)}></span>
+      </div>
+      {isShow && (
+        <div className={styles.itemsMenuHamburger}>
+          <ul>
+            {ItemsMenu.map(({ title, url }, index: number) => (
+              <li className={styles.itemsMenu} key={index}>
+                <Link
+                  href={url}
+                  // style={
+                  //   pathname === url
+                  // }
+                >
+                  {title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
   );
 }
--- utils/items-menu.ts
+++ utils/items-menu.ts
@@ -1,7 +1,7 @@
 import { Menu } from "../interfaces/Menu";
 
 export const ItemsMenu: Menu[] = [
-  { title: "Quem somos", url: "/" },
+  { title: "Quem somos", url: "/quem-somos" },
   { title: "Gastronomia", url: "/gastronomia" },
   { title: "Espaços", url: "/espacos" },
   { title: "Depoimentos", url: "/depoimentos" },
