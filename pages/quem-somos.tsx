587e1aa5d5143702d0565af226f5d90ceedef758
Barbara Hellen <barbara.pereira@modalgr.com.br>
2023-01-03 10:31:26 -0300

    feat: add quem somos resposivo

--- pages/quem-somos.tsx
+++ pages/quem-somos.tsx
@@ -6,7 +6,7 @@ import styles from "../styles/pages/quem-somos.module.scss";
 
 export default function QuemSomos() {
   return (
-    <>
+    <div className={styles.fullbody}>
       <Navbar />
       <div className={styles.backgroundQM}>
         <div className={styles.explicacao}>
@@ -26,10 +26,44 @@ export default function QuemSomos() {
             />
           </p>
 
-          <h1>Mais de 50 anos de festa!</h1>
+          <span>Mais de 50 anos de festa!</span>
+
+          <div className={styles.historia}>
+            <p>
+              Nascemos nos fundos de uma casa em São Paulo na década de 50,
+              vendendo por encomenda para a vizinhança, e hoje atuamos em
+              eventos sociais e empresariais para milhares de pessoas pelo
+              Brasil inteiro.
+            </p>
+            <p>
+              A paixão pela culinária fez com que nos especializássemos cada vez
+              mais, aperfeiçoando o que foi passado de geração em geração em uma
+              família aficionada pela cozinha, assim em 1970 foi fundado o
+              Buffet Cicareli.
+            </p>
+            <p>
+              Durante mais de 50 anos, realizamos os sonhos de muitas pessoas,
+              casamos muitas gerações, estes que se casaram fizeram as festas de
+              aniversário dos seus filhos, esses filhos se formaram, se casaram,
+              e nós estivemos presentes em todos esses momentos.
+            </p>
+            <p>
+              Assim é a nossa história, de geração para geração, e com muito
+              amor cuidando de cada festa como se fosse única, porque se pra
+              vocês é especial, para nós também é!
+            </p>
+          </div>
         </div>
-        <div className={styles.imagem}>asdas</div>
+        <div className={styles.imagem}>
+          <Image
+            src={"/images/rectangle19.webp"}
+            alt="Foto de uma mesa posta para o jantar"
+            width={470}
+            height={600}
+            // className={styles.responsiveImg}
+          />
+        </div>
       </div>
-    </>
+    </div>
   );
 }
