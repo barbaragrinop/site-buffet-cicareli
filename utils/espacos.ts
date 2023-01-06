e4dc57373d8c5cf7c440dc829df4a2933797b0d7
Barbara Hellen <barbara.pereira@modalgr.com.br>
2023-01-06 18:24:03 -0300

    feat: finaliza espacos

---
+++ utils/espacos.ts
@@ -0,0 +1,26 @@
+export const EspacosFake = [
+  {
+    img: "",
+    titulo: "Casa Grande",
+    descricao:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, voluptate veniam. Necessitatibus numquam nulla, similique optio quibusdam temporibus dolore placeat!",
+  },
+  {
+    img: "",
+    titulo: "Casa Grande",
+    descricao:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, voluptate veniam. Necessitatibus numquam nulla, similique optio quibusdam temporibus dolore placeat!",
+  },
+  {
+    img: "",
+    titulo: "Casa Grande",
+    descricao:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, voluptate veniam. Necessitatibus numquam nulla, similique optio quibusdam temporibus dolore placeat!",
+  },
+  {
+    img: "",
+    titulo: "Casa Grande",
+    descricao:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, voluptate veniam. Necessitatibus numquam nulla, similique optio quibusdam temporibus dolore placeat!",
+  },
+];
