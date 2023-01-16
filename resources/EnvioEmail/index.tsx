06e6cd5fe9e09fd0213843be18016ecbd608b164
Barbara Hellen <barbarapereira123@hotmail.com>
2023-01-16 19:58:40 -0300

    fix: sending email

--- resources/EnvioEmail/index.tsx
+++ resources/EnvioEmail/index.tsx
@@ -6,18 +6,18 @@ const transporter = nodemailer.createTransport({
     host: "smtp.mailtrap.io",
     port: 2525,
     auth: {
-      user: "63baff46d4afcd",
-      pass: "2fef2e98f951ed"
+        user: "63baff46d4afcd",
+        pass: "2fef2e98f951ed"
     }
-})
+});
 
 const emailHTML = render(<Email url="https://www.youtube.com/watch?v=mdiIQG5oBGk" />)
 
 const options = {
     from: "barbarapereira123@hotmail.com", 
     to: "babi.silva1163@gmail.com", 
-    subject: "Novidades pra sua conta."
+    subject: "Novidades pra sua conta.", 
+    html: emailHTML
 }
 
-
-transporter.sendMail((options)).then(() => console.log("Email enviado"))
+transporter.sendMail(options).then(() => console.log("Email enviado"))
