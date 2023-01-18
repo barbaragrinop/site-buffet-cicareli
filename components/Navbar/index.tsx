241ada7f8af391e8a142d3619eeb2e8e3b810531
Barbara Hellen <barbarapereira123@hotmail.com>
2023-01-18 19:50:12 -0300

    feat: remove sticky safari

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
   const { pathname, push } = useRouter();
 
   return (
-    <header className={styles.header} style={{ minWidth: "100%" }}>
+    <header className={styles.header}>
       <Image
         role="button"
         onClick={() => push("/")}
