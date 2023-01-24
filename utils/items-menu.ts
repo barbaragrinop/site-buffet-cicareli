3e57d186fd3fb81180b5016252cb21ab75dc890c
barbarahellen <50336676+barbaragrinop@users.noreply.github.com>
2023-01-24 10:49:01 -0300

    fix: hide espacos e depoimentos

--- utils/items-menu.ts
+++ utils/items-menu.ts
@@ -3,6 +3,6 @@ import { Menu } from "../interfaces/Menu";
 export const ItemsMenu: Menu[] = [
   { title: "Quem somos", url: "/quem-somos" },
   { title: "Gastronomia", url: "/gastronomia" },
-  { title: "Espaços", url: "/espacos" },
-  { title: "Depoimentos", url: "/depoimentos" },
+  // { title: "Espaços", url: "/espacos" },
+  // { title: "Depoimentos", url: "/depoimentos" },
 ];
